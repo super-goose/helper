31650840a399b2061651b5d85ecc5b63b7abacd7
Jay Davis <theworldneedssupergoose@gmail.com>
2022-07-24 10:20:09 -0500

    init commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>type a thing below</Text>
+      <TextInput style={styles.textInput} onChange={(e) => console.log(e.nativeEvent.text)} />
+      <StatusBar style="auto" />yar
     </View>
   );
 }
@@ -13,8 +14,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222',
+    color: '#dda',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#dda',
+    fontSize: 18,
+  },
+  textInput: {
+    color: '#dda',
+    fontSize: 18,
+    backgroundColor: '#666',
+    width: '90%',
+    padding: 9,
+  },
+
 });
